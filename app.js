3c2526395c3825fa0271e572224db8dc389a84de
Luka Savovic <luka.savovic199323@gmail.com>
2018-05-20 10:33:26 +0200

    done without axios

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+
 Vue.component('carditem', {
   props: ['card'],
   template: '<div class="card"><img v-bind:src="card.imgUrl" alt=""><p>{{card.phoneName}}</p></div>'
 });
 var app = new Vue({
   el: '#app',
-  
-  data () {
-    return {
-      info: null
-    }
+  data: {
+    url: "http://api.myjson.com/bins/1cpywi",
+    info: ""
   },
-  mounted () {
-    axios
-      .get('http://api.myjson.com/bins/1cpywi')
-      .then(response => (this.info = response.data.phones))
+  methods: {
+    ajax: function() {
+      var request = new XMLHttpRequest();
+      request.onload = function(vm) {
+          vm.info = this.response;
+          vm.info = JSON.parse(vm.info);
+      }.bind(request, this);
+      request.open("GET", this.url);
+      request.send();
+    }
+  }, beforeMount(){
+    this.ajax()
   }
-});
+
+  
+})
